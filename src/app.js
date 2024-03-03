695532fadd8d64335038cdfbcbdd98a6f3e5e1d0
Nikhila M <84263617+nikhila1612@users.noreply.github.com>
2024-03-03 17:23:32 +0530

    add

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
-const connectDB = require('./server/config/db');
-const { isActiveRoute } = require('./server/helpers/routeHelpers');
+const connectDB = require('../server/config/db');
+const { isActiveRoute } = require('../server/helpers/routeHelpers');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -42,8 +42,8 @@ app.set('view engine', 'ejs');
 app.locals.isActiveRoute = isActiveRoute; 
 
 
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'));
+app.use('/', require('../server/routes/main'));
+app.use('/', require('../server/routes/admin'));
 
 app.listen(PORT, ()=> {
   console.log(`App listening on port ${PORT}`);
